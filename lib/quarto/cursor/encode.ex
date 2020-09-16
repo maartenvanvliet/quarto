be86960f46efa0bc9969e4695bc3fb7ac0b6c52b
Maarten van Vliet <maartenvanvliet@users.noreply.github.com>
2020-09-16 12:37:47 +0200

    Fix example triggering error upon decoding erlang term
    If the app would try to decode the atom in the tuple it could fail because
    in some circumstances it would never have seen the atom before.

--- lib/quarto/cursor/encode.ex
+++ lib/quarto/cursor/encode.ex
@@ -7,11 +7,11 @@ defprotocol Quarto.Cursor.Encode do
 
   ```
   defimpl Quarto.Cursor.Encode, for: DateTime do
-    def convert(term), do: {:dt, DateTime.to_unix(term, :microsecond)}
+    def convert(term), do: {"dt", DateTime.to_unix(term, :microsecond)}
   end
 
   defimpl Quarto.Cursor.Decode, for: Tuple do
-    def convert({:dt, unix_timestamp}), do: DateTime.from_unix!(unix_timestamp, :microsecond)
+    def convert({"dt", unix_timestamp}), do: DateTime.from_unix!(unix_timestamp, :microsecond)
   end
   ```
 
