20bdfe715e8a99125de70932fed0b191bf6298f5
Maarten van Vliet <maartenvanvliet@gmail.com>
2022-01-11 17:37:23 +0100

    Bump version

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule Quarto.MixProject do
   def project do
     [
       app: :quarto,
-      version: "1.1.5",
+      version: "1.1.6",
       elixir: "~> 1.10",
       elixirc_paths: elixirc_paths(Mix.env()),
       start_permanent: Mix.env() == :prod,
